1b20f4ef365ff3c2841a54f492ea535b2e3911f5
Ed Powley <edpowley@gmail.com>
2018-04-07 11:45:18 +0100

    Added tournament AI list override

--- src/comp250/ListTournamentAIsInJar.java
+++ src/comp250/ListTournamentAIsInJar.java
@@ -2,6 +2,8 @@ package comp250;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.URL;
 import java.security.Policy;
@@ -14,7 +16,7 @@ import tournaments.LoadTournamentAIs;
 
 public class ListTournamentAIsInJar {
 	
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	public static void main(String[] args) throws IOException, ReflectiveOperationException {
 		
 		Policy.setPolicy(new SandboxSecurityPolicy());
 		System.setSecurityManager(new SecurityManager());
@@ -24,6 +26,17 @@ public class ListTournamentAIsInJar {
 
 		ClassLoader loader = new PluginClassLoader(new File(jarPath).toURI().toURL());
 
+		try {
+			Class<?> overrideClass = loader.loadClass("comp250.OverrideTournamentAIs");
+			Method method = overrideClass.getMethod("getClassNames");
+			String[] names = (String[]) method.invoke(null);
+			for (String name : names) {
+				System.out.println(name);
+			}
+			return;
+		} catch (ClassNotFoundException e) { // ignore the exception if OverrideTournamentAIs doesn't exist
+		}
+		
 		URL jar = new File(jarPath).toURI().toURL();
 		ZipInputStream zip = new ZipInputStream(jar.openStream());
 		while (true) {
